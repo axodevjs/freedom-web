4dd739f9116dd0ca922300aa9b737c84bea23717
axodevjs <axxxegao@gmail.com>
2024-11-10 17:00:20 +0500

    fix

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -21,9 +21,13 @@ export const Header: React.FC = () => {
   return (
     <header className="bg-white shadow">
       <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-        <h1 className="text-2xl font-bold flex flex-row items-center gap-1.5 text-gray-900">
-          Freedom <p className="text-primary">Hire</p>
-        </h1>
+        <Link href={"/"}>
+          {" "}
+          <h1 className="text-2xl font-bold flex flex-row items-center gap-1.5 text-gray-900">
+            Freedom <p className="text-primary">Hire</p>
+          </h1>
+        </Link>
+
         <nav>
           {isAuth ? (
             <DropdownMenu>
